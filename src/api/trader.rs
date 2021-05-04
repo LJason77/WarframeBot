d6f800fd86a6f7f88100984b451b404e9ce785cd
LJason <ljason@ljason.cn>
2021-05-04 19:14:36 +0800

    缓存奸商

--- src/api/trader.rs
+++ src/api/trader.rs
@@ -1,12 +1,37 @@
 //! 虚空商人
 
+use std::{
+	fs::{self, File},
+	io::Write,
+	path::Path,
+};
+
 use gettextrs::gettext;
 
 use crate::api;
 
 pub async fn get_trader() -> String {
-	let json = api::get_url("https://api.warframestat.us/pc/voidTrader").await;
-	let trader: crate::models::trader::Trader = serde_json::from_str(&json).unwrap();
+	// 读取缓存
+	let mut json = match fs::read_to_string("cache/trader.json") {
+		Ok(json) => json,
+		Err(_) => api::get_url("https://api.warframestat.us/pc/voidTrader").await,
+	};
+	let mut trader: crate::models::trader::Trader = serde_json::from_str(&json).unwrap();
+
+	if api::need_update(&trader.expiry) {
+		json = api::get_url("https://api.warframestat.us/pc/voidTrader").await;
+		trader = serde_json::from_str(&json).unwrap();
+	}
+
+	// 更新缓存
+	let json_file = Path::new("cache/trader.json");
+	let mut file = match File::create(&json_file) {
+		Err(error) => panic!("无法创建 {}：{}", json_file.display(), error),
+		Ok(file) => file,
+	};
+	if let Err(why) = file.write_all(&json.as_bytes()) {
+		println!("更新缓存失败：{}", why)
+	}
 
 	let mut trader_str = format!("{}\n", gettext(trader.location));
 	if !trader.active {
